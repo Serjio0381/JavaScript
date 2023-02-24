aa91735e5ac837a5f353cf9a6c23c2579307a5df
Sergey <optimtag@mail.ru>
2023-02-24 14:50:03 +0300

    Десятая Домашка JSON

---
+++ HW_10_JSON/data.js
@@ -0,0 +1,20 @@
+const dataPokemon = `{
+                    "abilities": [
+                                    {
+                                        "ability": {
+                                                    "name": "limber",
+                                                    "url": "https://pokeapi.co/api/v2/ability/7/"
+                                                    },
+                                        "is_hidden": false,
+                                        "slot": 1
+                                    },
+                                    {
+                                        "ability": {
+                                                    "name": "imposter",
+                                                    "url": "https://pokeapi.co/api/v2/ability/150/"
+                                                    },
+                                        "is_hidden": true,
+                                        "slot": 3
+                                    }
+                                ],
+                        "base_experience": 101}`
