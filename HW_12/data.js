845dcdb216e31a5e30f0fb2f7473fccdcb9d9565
Sergey <optimtag@mail.ru>
2023-03-01 18:45:41 +0300

    Двенадцатая Домашка

---
+++ HW_12/data.js
@@ -0,0 +1,18 @@
+const dataVideo = `[
+{
+    "name" : "video1",
+    "src" : "https://mazwai.com/videvo_files/video/free/2016-11/small_watermarked/Smoke_Light_08_Videvo_preview.webm",
+    "desctiption" : "красивое видео"
+
+},
+{
+    "name" : "video2",
+    "src" : "https://mazwai.com/videvo_files/video/free/2015-04/small_watermarked/Ocean_Waves_slow_motion_videvo_preview.webm",
+    "desctiption" : "интересный ролик"
+},
+{
+    "name" : "video3",
+    "src" : "https://mazwai.com/videvo_files/video/free/2015-06/small_watermarked/Raindrops_Macro2_Videvo_preview.webm",
+    "desctiption" : "прикольный видосик"
+}
+]`
